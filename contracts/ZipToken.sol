1701de53ce4accf39b3c8d3ed4920c2c241c1f77
Carsten Munk <carsten.munk@zipperglobal.com>
2018-02-25 17:02:41 +0100

    Final ZIPT
    Signed-off-by: Carsten Munk <carsten.munk@zipperglobal.com>

--- contracts/ZipToken.sol
+++ contracts/ZipToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.17;
 import '../node_modules/zeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
 
 contract ZipToken is PausableToken {
-    string public constant name = "ZIPT Convertible";
-    string public constant symbol = "ZIPTC";
+    string public constant name = "Zipper Token";
+    string public constant symbol = "ZIPT";
     uint8 public constant decimals = 18;
     uint public constant TOTAL_TOKEN_AMOUNT = 1000000000;
     uint public constant INITIAL_SUPPLY = TOTAL_TOKEN_AMOUNT * 10**uint(decimals);
@@ -24,9 +24,5 @@ contract ZipToken is PausableToken {
                 transfer(a, v);
             }
         }
-    }
-
-    function die() public onlyOwner {
-      selfdestruct(msg.sender);
     }
 }
